f80315565359e92bdbce4e973213b838855f07e0
pauline-banye <paulinebanye@gmail.com>
2022-04-05 15:45:58 +0100

    create erc20

--- contracts/NestCoin.sol
+++ contracts/NestCoin.sol
@@ -5,8 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NestCoin is ERC20, Ownable {
-    constructor() ERC20("NestCoin", "NCT") {}
+    constructor(
+        
+    ) ERC20("NestCoin", "NCT") {}
 
+    // mint a supply of 10000 tokens
     function mint() public onlyOwner {
         _mint(msg.sender, 10000 * 10 ** 18);
     }
