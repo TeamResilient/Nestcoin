811e8f166f6c580fc930befe7eed647507e5bf6d
JMario1 <jdonmarie@gmail.com>
2022-04-06 09:01:46 +0100

    fix batch transfer

--- packages/hardhat/contracts/Exchange.sol
+++ packages/hardhat/contracts/Exchange.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
 import "./Nestcoin.sol";
 
 
@@ -13,9 +14,10 @@ contract Nxt {
         nestcoin = NestCoin(tokenAddr);
     }
 
-     function batchTokenTransfer(address[] memory _userAddr,  uint256[] memory _amount) public  {
+     function batchTokenTransfer(address[] memory _userAddr,  uint256[] memory _amount) public  onlyOwner {
         require(_userAddr.length == _amount.length, "Number of Addresses must match amount");
         require(_userAddr.length <= 200, "Array must not be greater than 200");
+        require(_amount >= address(this).balance, "Not enough nestcoin to send");
         for (uint256 i = 0; i < _userAddr.length; i++) {
             if(address(_userAddr[i] != address(0))){
                 nestcoin.transfer(userAddr[i], _amount[i]);
