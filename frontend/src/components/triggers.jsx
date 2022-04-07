bc0633a22027b76e899c1db2e8d051a446115ce2
taofeek-ob <taofeek01@yahoo.com>
2022-04-07 17:52:26 +0100

    hi

--- frontend/src/components/triggers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const triggers = () => {
-  return (
-    <div>triggers</div>
-  )
-}
-
-export default triggers
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./NestcoinToken.json";
+
+export const contractAddress = "0xc62f2cea09f8d05e4c774c0aac433a6cb37d3c82";
+export const contractABI = abi.abi;
