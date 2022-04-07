d79514dfd77668fe3f99438e86f21bf600727ca3
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2022-04-07 18:01:33 +0100

    Reworked test for updated Contract

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ let contract;
 
 describe("NestCoin", function(){
     it("should be deployed to the testnet", async function(){
-        const NestcoinContract = await ethers.getContractFactory("NestCoin");
+        const NestcoinContract = await ethers.getContractFactory("NestcoinReward");
         const [ owner ] = await ethers.getSigners();
         const deployedNestcoinContract = await NestcoinContract.connect(owner).deploy();
         contract = await deployedNestcoinContract.deployed();
@@ -15,27 +15,47 @@ describe("NestCoin", function(){
 });
 
 describe("Admins()", function(){
-    it("Should be able to add, get, check, remove Admins", async function(){
-        const [ owner,secondAccount ] = await ethers.getSigners();
+    it("Should be able to add, remove Admins", async function(){
+        const [ owner, secondAccount, thirdAccount] = await ethers.getSigners();
         console.log('\t', " Admin Address: ", owner.address);
-        await contract.addAdmin(owner.address);
-        const admins = await contract.getAdmin();
-        expect(admins[0]).to.equal(owner.address);
-        const checkAdmin = await contract.checkAdmin(owner.address);
-        console.log(checkAdmin);
-        expect(checkAdmin).to.equal(true);
-        
+        assignAdmin = await contract.connect(owner).assignAdmin(secondAccount.address);
+        const txResult = await assignAdmin.wait();
+        expect(txResult.status).to.equal(1);
+        removeAdmin = await contract.connect(owner).removeAdmin(secondAccount.address);
+        const txResult2 = await removeAdmin.wait();
+        expect(txResult2.status).to.equal(1);
+        console.log("Revert if address is not an admin");
+        await expect(contract.connect(owner).removeAdmin(thirdAccount.address)).to.be.revertedWith("Address is not an admin.");
+    
     })
 });
 
-describe("Airdrop", function(){
+describe("Reward", function(){
+
+    it("Admin that has been removed should not be able to reward", async function(){
+        const [ owner, secondAccount] = await ethers.getSigners();
+        await contract.connect(owner).assignAdmin(secondAccount.address);
+        await contract.connect(owner).removeAdmin(secondAccount.address);
+        await expect(contract.connect(secondAccount).dispatchRewards(['0x70997970c51812dc3a010c7d01b50e0d17dc79c8','0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc'] ,[2,2])).to.be.revertedWith("Only Admin Has Access!");
+
+    });
+
+    it("Should not be able to reward if loyal customer array and amount array are not equal.",async function(){
+        const [ owner, secondAccount] = await ethers.getSigners();
+        await contract.connect(owner).assignAdmin(secondAccount.address);
+        await contract.connect(owner).removeAdmin(secondAccount.address);
+        await expect(contract.connect(owner).dispatchRewards(['0x70997970c51812dc3a010c7d01b50e0d17dc79c8','0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc'] ,[2])).to.be.revertedWith("Array Lengths must be equal.");
+    });
+
+
     it("Should be able to airdrop tokens to addresses", async function(){
         const [ owner] = await ethers.getSigners();
-        const NestcoinContract2 = await ethers.getContractFactory("NestCoin");
-        const deployedNestcoinContract2 = await NestcoinContract2.connect(owner).deploy();
-        const contract2 = await deployedNestcoinContract2.deployed();
-        const airdrop = await contract2.connect(owner).airdrop(['0x70997970c51812dc3a010c7d01b50e0d17dc79c8','0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc'] ,[2,2]);
+        const airdrop = await contract.connect(owner).dispatchRewards(['0x70997970c51812dc3a010c7d01b50e0d17dc79c8','0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc'] ,[2,2]);
         const result = await airdrop.wait();
         expect(result.status).to.equal(1);
-    })
+    });
+
+    
+
+
 });
