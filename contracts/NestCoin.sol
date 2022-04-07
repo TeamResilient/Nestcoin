b1d83c610861ac33cf5b22fe64260e6063764505
Taofeek Bakare <taofeek01@yahoo.com>
2022-04-07 14:07:33 +0100

    Update NestCoin.sol

--- contracts/NestCoin.sol
+++ contracts/NestCoin.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract NestcoinReward is ERC20  {
+contract NestcoinReward is ERC20, Ownable  {
     //track nestcoin admins
     mapping(address=>bool) private admins; 
 
@@ -21,7 +21,7 @@ contract NestcoinReward is ERC20  {
 
 
     constructor() ERC20("Nestcoin","NCT"){
-        _mint(msg.sender, 1000000 * 10 ** 18);
+     
         admins[msg.sender] = true;
     }
   
